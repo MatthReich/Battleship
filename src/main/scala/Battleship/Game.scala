b1a9ebf5e5a0239390335d4f00fa05b14f18e941
Matthias Reichenbach <matthreich@gmail.com>
2019-10-11 13:43:21 +0200

    updated

--- src/main/scala/Battleship/Game.scala
+++ src/main/scala/Battleship/Game.scala
@@ -4,8 +4,15 @@ package Battleship
 object Battleship {
   def main(args: Array[String]): Unit = {
 
-    val player = model.Player("Matth")
-    println("Battleship \n played by: " + player)
+    val player1 = model.Player("Marcel")
+    val player2 = model.Player("Matthias")
+
+    println(
+      "Battleship \n\n" +
+      "played by: \n" + "" +
+      "\t: " + player1 + "\n" +
+      "\t: " + player2 + "\n"
+    )
   }
 
 }

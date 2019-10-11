07a53a3244042449d7bb2e406babed21631a1122
Matthias Reichenbach <matthreich@gmail.com>
2019-10-11 11:37:44 +0200

    created new

---
+++ src/main/scala/GameBasiscs/package.scala
@@ -0,0 +1,10 @@
+package GameBasiscs
+
+object `package` {
+
+  def main(args: Array[String]): Unit = {
+    val x = 5
+    println("funktioniert premium " + x)
+  }
+
+}
---
+++ src/main/scala/GameBasiscs/ship.java
@@ -0,0 +1,4 @@
+package GameBasiscs;
+
+public class ship {
+}

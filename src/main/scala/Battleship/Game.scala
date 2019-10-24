05bd4c5024a7412e317cfcdd727535da9fbcd8e6
Matthias Reichenbach <matthreich@gmail.com>
2019-10-24 13:15:25 +0200

    added creator to mainprint

--- src/main/scala/Battleship/Game.scala
+++ src/main/scala/Battleship/Game.scala
@@ -6,13 +6,18 @@ object Battleship {
     val player_01 = model.Player("Marcel")
     val player_02 = model.Player("Matthias")
 
+    val creator_01 = model.Creator("Marcel")
+    val creator_02 = model.Creator("Matthias")
+
     print(
       "#" * 20 + "\n" +
         "Battleship \n\n" +
+        "Player One: " + player_01 + "\n" +
+        "Player One: " + player_02 + "\n" +
         "#" * 20 + "\n" +
         "created by: \n" + "" +
-        "\t: " + player_01 + "\n" +
-        "\t: " + player_02 + "\n" +
+        "\t: " + creator_01 + "\n" +
+        "\t: " + creator_02 + "\n" +
         "#" * 20 + "\n"
     )
   }

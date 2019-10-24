a3f417987b3b15e1933f6249406ac675df789028
Marcelg1235 <Marcelg1235@gmail.com>
2019-10-24 10:44:35 +0200

    rename val creator* to player*

--- src/main/scala/Battleship/Game.scala
+++ src/main/scala/Battleship/Game.scala
@@ -3,16 +3,16 @@ package Battleship
 object Battleship {
   def main(args: Array[String]): Unit = {
 
-    val creator_01 = model.Creator("Marcel")
-    val creator_02 = model.Creator("Matthias")
+    val player_01 = model.Player("Marcel")
+    val player_02 = model.Player("Matthias")
 
     print(
       "#" * 20 + "\n" +
         "Battleship \n\n" +
         "#" * 20 + "\n" +
         "created by: \n" + "" +
-        "\t: " + creator_01 + "\n" +
-        "\t: " + creator_02 + "\n" +
+        "\t: " + player_01 + "\n" +
+        "\t: " + player_02 + "\n" +
         "#" * 20 + "\n"
     )
   }

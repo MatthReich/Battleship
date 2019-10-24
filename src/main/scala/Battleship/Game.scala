80ba82a2695702aaa9eda5c064e435ebd203d9e4
Matthias Reichenbach <matthreich@gmail.com>
2019-10-24 15:30:42 +0200

    added field implementation and test

--- src/main/scala/Battleship/Game.scala
+++ src/main/scala/Battleship/Game.scala
@@ -1,6 +1,6 @@
 package Battleship
 
-object Battleship {
+object Game {
   def main(args: Array[String]): Unit = {
 
     val creator_01 = model.Creator("Marcel")
@@ -17,15 +17,23 @@ object Battleship {
     )
 
     print("Please insert playername for player_01\n")
-    val player_01 = scala.io.StdIn.readLine()
+    val player_01 = model.Player(scala.io.StdIn.readLine().toString)
     print("Please insert playername for player_02\n")
-    val player_02 = scala.io.StdIn.readLine()
+    val player_02 = model.Player(scala.io.StdIn.readLine().toString)
 
     print(
       "Actual playerconfiguration: \n" +
     "\tPlayer One: " + player_01 + "\n" +
       "\tPlayer Two: " + player_02 + "\n"
     )
+
+    val field = new model.fieldTest
+    field.insertMap("A0", false)
+    field.insertMap("A1", true)
+    field.insertMap("A2", false)
+    field.getKey("A1")
+    field.getKey("A0")
+    field.print()
   }
 
 }

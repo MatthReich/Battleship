501d0b1716c84da44fa8b35f50fc24e2f83c3ec5
Matthias Reichenbach <matthreich@gmail.com>
2019-10-24 13:28:20 +0200

    added player readin from terminal and new output

--- src/main/scala/Battleship/Game.scala
+++ src/main/scala/Battleship/Game.scala
@@ -3,22 +3,28 @@ package Battleship
 object Battleship {
   def main(args: Array[String]): Unit = {
 
-    val player_01 = model.Player("Marcel")
-    val player_02 = model.Player("Matthias")
-
     val creator_01 = model.Creator("Marcel")
     val creator_02 = model.Creator("Matthias")
 
     print(
       "#" * 20 + "\n" +
-        "Battleship \n\n" +
-        "Player One: " + player_01 + "\n" +
-        "Player One: " + player_02 + "\n" +
+        "    Battleship \n" +
         "#" * 20 + "\n" +
         "created by: \n" + "" +
         "\t: " + creator_01 + "\n" +
         "\t: " + creator_02 + "\n" +
-        "#" * 20 + "\n"
+        "#" * 20 + "\n\n\n"
+    )
+
+    print("Please insert playername for player_01\n")
+    val player_01 = scala.io.StdIn.readLine()
+    print("Please insert playername for player_02\n")
+    val player_02 = scala.io.StdIn.readLine()
+
+    print(
+      "Actual playerconfiguration: \n" +
+    "\tPlayer One: " + player_01 + "\n" +
+      "\tPlayer Two: " + player_02 + "\n"
     )
   }
 

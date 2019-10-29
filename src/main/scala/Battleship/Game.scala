6e2ed044d6af59ad356c2b8c3f06b22c7931e0b1
MatthReich <matthreich@gmail.com>
2019-10-29 14:38:06 +0100

    added implementation fo field

--- src/main/scala/Battleship/Game.scala
+++ src/main/scala/Battleship/Game.scala
@@ -1,5 +1,7 @@
 package Battleship
 
+import Battleship.model.PlayingField
+
 object Game {
   def main(args: Array[String]): Unit = {
 
@@ -23,18 +25,26 @@ object Game {
 
     print(
       "Actual playerconfiguration: \n" +
-    "\tPlayer One: " + player_01 + "\n" +
-      "\tPlayer Two: " + player_02 + "\n"
+      "\tPlayer One: " + player_01 + "\n" +
+      "\tPlayer Two: " + player_02 + "\n\n"
     )
 
-    val field = new model.fieldTest(player_01)
-    field.insertMap("A0", 0,false)
-    field.insertMap("A0", 1,true)
-    field.insertMap("A1", 0,true)
-    field.insertMap("A2", 0,false)
-    field.getKey("A1")
-    field.getKey("A2")
-    field.print()
+    print("Please insert fieldsize\n" +
+          "options: insert 10 for [10 x 10] field\n"
+    )
+    val fieldSize: Int = scala.io.StdIn.readLine().toInt
+
+    val fieldP_01 = PlayingField(player_01, fieldSize)
+    val fieldP_02 = PlayingField(player_02, fieldSize)
+
+    print("\n")
+
+    val outputP_01 = fieldP_01.printField()
+    print(outputP_01 + "\n")
+
+    val outputP_02 = fieldP_02.printField()
+    print(outputP_02 + "\n")
+
   }
 
 }

efc06cf9afb31b48aa2d6b1d416d089d496d0074
MatthReich <matthreich@gmail.com>
2019-10-29 17:31:21 +0100

    updated

--- src/test/scala/Battleship/model/CreatorSpec.scala
+++ src/test/scala/Battleship/model/CreatorSpec.scala
@@ -6,12 +6,17 @@ class CreatorSpec extends WordSpec with Matchers {
   "A Creator" when {
     "new" should {
       val creator = Creator("Your Name")
+
+      "get coverage" in {
+        Creator.unapply(creator).get should be ("Your Name")
+      }
+
       "have a name" in {
         creator.name should be("Your Name")
-        creator.toString should be("Your Name")
       }
+
       "have a nice String representation" in {
-        Matchers.convertToStringShouldWrapper(creator.toString) should be("Your Name")
+        creator.toString should be("Your Name")
       }
     }
   }
--- src/test/scala/Battleship/model/PlayerFieldTest.scala
+++ src/test/scala/Battleship/model/PlayerFieldTest.scala
@@ -7,22 +7,34 @@ class PlayerFieldTest extends WordSpec with Matchers {
   "A PlayerField" when {
     "new" should {
 
-    val player = Player("Your Name")
-    val size = 10
-    val fieldP_01 = PlayerField(player, size)
+      val player = Player("Your Name")
+      val size = 10
+      val fieldP_01 = PlayerField(player, size)
 
-      "contains one Of" in {
-
+      "get coverage" in {
+        PlayerField.unapply(fieldP_01).get should be (("Your Name", 10))
       }
 
-    "replaceEntry" in {
-        fieldP_01.replaceEntry(0,0,true)
-    }
+      "replaceEntry" in {
+        fieldP_01.replaceEntry(0, 0, true)
+        fieldP_01.listA0(0) should be(true)
+
+        fieldP_01.replaceEntry(0, 0, false)
+        fieldP_01.listA0(0) should be(false)
+      }
+
+      "getEntry" in {
+        fieldP_01.getEntry(0, 0) should be(fieldP_01.listA0(0))
+      }
 
       "printField" in {
         val string = fieldP_01.printField()
-        string should startWith ("Field of: " + player)
-        string should endWith ("END OF FIELD\n")
-     }
-  }}
+        string should startWith("Field of: " + player)
+        string should endWith("\n")
+        string should include("A0")
+        string should include("A5")
+        string should include("A9")
+      }
+    }
+  }
 }

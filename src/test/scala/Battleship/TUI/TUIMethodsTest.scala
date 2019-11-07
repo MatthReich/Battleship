dcb307a1b032e736ac97c6c3f6f030ad9b38431e
Marcelg1235 <Marcelg1235@gmail.com>
2019-11-07 18:28:45 +0100

    Add getRow Test to TUIMethods Test file

--- src/test/scala/Battleship/TUI/TUIMethodsTest.scala
+++ src/test/scala/Battleship/TUI/TUIMethodsTest.scala
@@ -7,22 +7,25 @@ import org.scalatest.{Matchers, WordSpec}
 
 @RunWith(classOf[JUnitRunner])
 class TUIMethodsTest extends WordSpec with Matchers {
-  "Methods" when {
-    "new" should {
-      "printWelcome" in {
-      }
-      "setPlayer" in {
-      }
-      "printGetPlayer" in {
-      }
-      "printSetField" in {
-      }
-      "askShips" in {
-      }
-      "printField" in {
-      }
-      "getRow" in {
-      }
+  "printWelcome" in {
+  }
+  "setPlayer" in {
+  }
+  "printGetPlayer" in {
+  }
+  "printSetField" in {
+  }
+  "askShips" in {
+  }
+  "printField" in {
+  }
+  "getRow" in {
+    val testStrings: List[String] = List("A0","A1","A2","A3", "A4","A5","A6","A7","A8","A9")
+    var idx = 0
+    for (elem <- testStrings) {
+      val testint = TUIMethods.getRow(elem)
+      testint should be(idx)
+      idx += 1
     }
   }
 }

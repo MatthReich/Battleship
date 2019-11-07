473c4f178f8aab82dc04b7220dbc484db8baf399
Matthias <matthreich@gmail.com>
2019-11-07 22:25:26 +0100

    updated loop, simple gameplay without failure is possible

--- src/main/scala/Battleship/TUI/TUIInterface.scala
+++ src/main/scala/Battleship/TUI/TUIInterface.scala
@@ -10,7 +10,7 @@ object TUIInterface {
   var player_02 = Player("")
   var fieldSize: Int = 0
 
-
+  //noinspection ScalaStyle
   def execute(): Unit = {
     print(printWelcome())
     setPlayer()
@@ -34,20 +34,52 @@ object TUIInterface {
     // printField(fieldP_01, 1)
     // printField(fieldP_02, 2)
 
+
+    var turn = true
     while (true) {
+      if (turn) print(player_01 + " is on turn\n")
+      if (!turn) print(player_02.name + " is on turn\n")
+
       val input = scala.io.StdIn.readLine().toString.split(" ")
+
       if (input.length == 1) {
         input(0) match {
-
           case "getPlayerStats" => print(printGetPlayer())
           case "q" => return
           case "exit" => return
           case _ => AnyRef
         }
+
+        if (turn) turn = false
+        else turn = true
+
       } else {
-        print("---\n")
+        val row = getRow(input(0))
+        val column = input(1).toInt
+
+        if (turn) { // player one guessing
+          val guess = fieldP_02_Ships.getEntry(row, column)
+          if (guess) {
+            fieldP_02.replaceEntry(row, column, true)
+            print(printField(fieldP_02, 2))
+          }
+          if (!turn) turn = true
+          else turn = false
+
+        } else {
+          val guess = fieldP_01_Ships.getEntry(row, column)
+          if (guess) {
+            fieldP_01.replaceEntry(row, column, true)
+            print(printField(fieldP_01, 1))
+          }
+          if (!turn) turn = true
+          else turn = false
+        }
       }
     }
+
   }
 }
 
+// when ship is sunk -> turn colour
+// player sets first location then have to select one we created

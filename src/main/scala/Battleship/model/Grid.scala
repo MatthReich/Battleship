02722fff9683ec6935c83069932ad77c486857b0
Marcelg1235 <Marcelg1235@gmail.com>
2019-11-12 16:05:04 +0100

    Add list in grid class

--- src/main/scala/Battleship/model/Grid.scala
+++ src/main/scala/Battleship/model/Grid.scala
@@ -1,3 +1,21 @@
 package Battleship.model
 
-case class Grid()
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+case class Grid(size: Int){
+
+  val listA0: ListBuffer[Boolean] = ListBuffer.fill(size)(false)
+  val listA1: ListBuffer[Boolean] = ListBuffer.fill(size)(false)
+  val listA2: ListBuffer[Boolean] = ListBuffer.fill(size)(false)
+  val listA3: ListBuffer[Boolean] = ListBuffer.fill(size)(false)
+  val listA4: ListBuffer[Boolean] = ListBuffer.fill(size)(false)
+  val listA5: ListBuffer[Boolean] = ListBuffer.fill(size)(false)
+  val listA6: ListBuffer[Boolean] = ListBuffer.fill(size)(false)
+  val listA7: ListBuffer[Boolean] = ListBuffer.fill(size)(false)
+  val listA8: ListBuffer[Boolean] = ListBuffer.fill(size)(false)
+  val listA9: ListBuffer[Boolean] = ListBuffer.fill(size)(false)
+
+
+
+}

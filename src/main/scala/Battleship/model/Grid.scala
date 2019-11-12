27baa1a657d98d2fab21c8caa899336dc5fa275d
Marcelg1235 <Marcelg1235@gmail.com>
2019-11-12 16:29:08 +0100

    Add setShips Method

--- src/main/scala/Battleship/model/Grid.scala
+++ src/main/scala/Battleship/model/Grid.scala
@@ -3,6 +3,11 @@ package Battleship.model
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
+//Standart  0
+//Schiff    1
+//Treffer   2
+//Wasser    3
+
 case class Grid(size: Int){
   val matrix = Array.ofDim[Int](size,size)
 
@@ -12,6 +17,22 @@ case class Grid(size: Int){
 
   def setField(x: Int, y: Int, value: Int): Unit = {
     matrix(x)(y) = value
+  }
+
+  def setShip(x: Int, y: Int, x2: Int, y2: Int): Unit = {
+    if ((x2 - x) > (y2 - y)) {
+      var tmp = x2
+      while (x <= tmp) {
+        matrix(tmp)(y) = 1
+        tmp -= 1
+      }
+    } else {
+      var tmp = y2
+      while (y <= tmp) {
+        matrix(x)(tmp) = 1
+        tmp -= 1
+      }
+    }
   }
 
   def getSize: Int = {

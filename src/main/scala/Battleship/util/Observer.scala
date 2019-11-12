33a56b4b74ed24475a63d3a789d1087300b7750e
Marcelg1235 <Marcelg1235@gmail.com>
2019-11-12 15:19:39 +0100

    change val to var

--- src/main/scala/Battleship/util/Observer.scala
+++ src/main/scala/Battleship/util/Observer.scala
@@ -5,7 +5,7 @@ trait Observer {
 }
 
 class Observable {
-  val subscribers: Vector[Observer] = Vector()
+  var subscribers: Vector[Observer] = Vector()
   def add(s: Observer): Unit = subscribers = subscribers :+ s
   def remove(s: Observer): Unit = subscribers = subscribers.filterNot(o => o == s)
   def notifyObservers: Unit = subscribers.foreach(o => o.update)

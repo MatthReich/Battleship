857f1de42da199d0c029a5d627d3b8dc53839958
Marcelg1235 <Marcelg1235@gmail.com>
2019-11-12 16:31:26 +0100

    Delete setMethod

--- src/main/scala/Battleship/controller/Controller.scala
+++ src/main/scala/Battleship/controller/Controller.scala
@@ -4,15 +4,11 @@ import Battleship.model.Grid
 import Battleship.util.Observable
 
 class Controller(var grid:Grid) extends Observable{
-  def createEmptyGrid(size: Int):Unit = {
+  def createGrid(size: Int):Unit = {
     grid = new Grid(size);
     notifyObservers
   }
 
   def gridToString: String = grid.toString
 
-  def set(row: Int, col: Int, value: Int):Unit = {
-    grid = grid.set(row, col, value)
-    notifyObservers
-  }
 }

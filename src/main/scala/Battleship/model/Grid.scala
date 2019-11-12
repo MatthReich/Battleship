e84819816474afb72fd1082724cf874f29a4c3c4
Marcelg1235 <Marcelg1235@gmail.com>
2019-11-12 16:11:22 +0100

    change all lists to one 2D Array

--- src/main/scala/Battleship/model/Grid.scala
+++ src/main/scala/Battleship/model/Grid.scala
@@ -5,16 +5,7 @@ import scala.collection.mutable.ListBuffer
 
 case class Grid(size: Int){
 
-  val listA0: ListBuffer[Boolean] = ListBuffer.fill(size)(false)
-  val listA1: ListBuffer[Boolean] = ListBuffer.fill(size)(false)
-  val listA2: ListBuffer[Boolean] = ListBuffer.fill(size)(false)
-  val listA3: ListBuffer[Boolean] = ListBuffer.fill(size)(false)
-  val listA4: ListBuffer[Boolean] = ListBuffer.fill(size)(false)
-  val listA5: ListBuffer[Boolean] = ListBuffer.fill(size)(false)
-  val listA6: ListBuffer[Boolean] = ListBuffer.fill(size)(false)
-  val listA7: ListBuffer[Boolean] = ListBuffer.fill(size)(false)
-  val listA8: ListBuffer[Boolean] = ListBuffer.fill(size)(false)
-  val listA9: ListBuffer[Boolean] = ListBuffer.fill(size)(false)
+  val matrix = Array.ofDim[Int](size,size)
 
   val playingField: mutable.Map[String, ListBuffer[Boolean]] = mutable.Map(
     "A0" -> listA0,

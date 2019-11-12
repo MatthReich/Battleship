0438b655ef9a84e87463ffbb1aa48063a23e4a25
Marcelg1235 <Marcelg1235@gmail.com>
2019-11-12 16:05:41 +0100

    AddMethod playing Field in grid class

--- src/main/scala/Battleship/model/Grid.scala
+++ src/main/scala/Battleship/model/Grid.scala
@@ -16,6 +16,19 @@ case class Grid(size: Int){
   val listA8: ListBuffer[Boolean] = ListBuffer.fill(size)(false)
   val listA9: ListBuffer[Boolean] = ListBuffer.fill(size)(false)
 
+  val playingField: mutable.Map[String, ListBuffer[Boolean]] = mutable.Map(
+    "A0" -> listA0,
+    "A1" -> listA1,
+    "A2" -> listA2,
+    "A3" -> listA3,
+    "A4" -> listA4,
+    "A5" -> listA5,
+    "A6" -> listA6,
+    "A7" -> listA7,
+    "A8" -> listA8,
+    "A9" -> listA9
+  )
+
 
 
 }

6873584e6435c662e393bc9360e9c02bc7ed2877
Marcelg1235 <Marcelg1235@gmail.com>
2019-11-13 16:43:28 +0100

    Delete useless getRowTest Method

--- src/test/scala/Battleship/TUI/TUIMethodsTest.scala
+++ src/test/scala/Battleship/TUI/TUIMethodsTest.scala
@@ -19,14 +19,4 @@ class TUIMethodsTest extends WordSpec with Matchers {
   "askShips" in {
     // can't be tested
   }
-
-  "getRow" in {
-    val testStrings: List[String] = List("A0","A1","A2","A3", "A4","A5","A6","A7","A8","A9")
-    var idx = 0
-    for (elem <- testStrings) {
-      val testInt = TUIMethods.getRow(elem)
-      testInt should be(idx)
-      idx += 1
-    }
-  }
 }

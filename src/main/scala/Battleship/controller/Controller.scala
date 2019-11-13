1dd3a088f09f4922f1ce6bdc8f926a866bbff93a
Marcelg1235 <Marcelg1235@gmail.com>
2019-11-13 16:08:10 +0100

    Add Variables to Controller

--- src/main/scala/Battleship/controller/Controller.scala
+++ src/main/scala/Battleship/controller/Controller.scala
@@ -1,14 +1,18 @@
 package Battleship.controller
 
-import Battleship.model.Grid
+import Battleship.TUI.{TUIInterface, TUIMethods}
+import Battleship.model.{Creator, Grid, Player}
 import Battleship.util.Observable
 
-class Controller(var grid:Grid) extends Observable{
-  def createGrid(size: Int):Unit = {
-    grid = new Grid(size);
-    notifyObservers
-  }
+class Controller() extends Observable{
 
-  def gridToString: String = grid.toString
+  val creator_01: Creator = Creator("Marcel")
+  val creator_02: Creator = Creator("Matthias")
+  var player_01 = Player("")
+  var player_02 = Player("")
+  var fieldSize: Int = 10
+  var playerGrid_01: Grid = new Grid(fieldSize)
+  var playerGrid_02: Grid = new Grid(fieldSize)
+
 
 }

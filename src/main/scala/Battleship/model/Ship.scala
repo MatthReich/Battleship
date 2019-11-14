75d4ecf60140821c92055716bde839b4420f73a3
Marcelg1235 <Marcelg1235@gmail.com>
2019-11-14 17:02:12 +0100

    Add setCoordinates Method

--- src/main/scala/Battleship/model/Ship.scala
+++ src/main/scala/Battleship/model/Ship.scala
@@ -4,5 +4,27 @@ case class Ship(shipCoordinates: Array[Int]) {
   var size = 0
   var coordinats = Array.ofDim[Int](5, 2)
 
+  def setCoordinates(): Unit = {
+    if (shipCoordinates(0) == shipCoordinates(2)) {
+      size = shipCoordinates(3) - shipCoordinates(1)
+      coordinats = Array.ofDim[Int](size, 2)
+      var idy = 0
+      while (idy != size) {
+        coordinats(idy)(0) = shipCoordinates(0)
+        coordinats(idy)(1) = shipCoordinates(1 + idy)
+        idy += 1
+      }
+    } else {
+      size = shipCoordinates(2) - shipCoordinates(0)
+      coordinats = Array.ofDim[Int](size, 2)
+      var idy = 0
+      while (idy != size) {
+        coordinats(idy)(0) = shipCoordinates(0 + idy)
+        coordinats(idy)(1) = shipCoordinates(1)
+        idy += 1
+      }
+    }
+  }
+
 
 }

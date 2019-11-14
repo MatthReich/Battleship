d7361175ece25a08cba0dfdd4a73d0992da24b6c
Marcelg1235 <Marcelg1235@gmail.com>
2019-11-14 16:58:48 +0100

    Change things in Test file

--- src/test/scala/Battleship/model/GridSpec.scala
+++ src/test/scala/Battleship/model/GridSpec.scala
@@ -26,16 +26,6 @@ class GridSpec extends WordSpec with Matchers {
         val tmp = grid.getField(0, 0)
         tmp should be(1)
       }
-
-      "setShip" in {
-        grid.setShip(0, 0, 1, 0)
-        var tmp = grid.getField(0, 0)
-        tmp should be(1)
-        grid.setShip(2, 2, 2, 3)
-        tmp = grid.getField(2, 2)
-        tmp should be(1)
-      }
-
     }
   }
 

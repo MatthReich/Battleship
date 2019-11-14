c14b33752a9268c26b3dadb57b07cd843405fc16
Matthias <matthreich@gmail.com>
2019-11-14 13:52:12 +0100

    added tests

--- src/test/scala/Battleship/model/ShipSpec.scala
+++ src/test/scala/Battleship/model/ShipSpec.scala
@@ -7,4 +7,23 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class ShipSpec extends WordSpec with Matchers {
 
+  "A Ship" when {
+    "new" should {
+      val coordinats = Array(1, 2, 1, 3)
+      val ship = Ship(coordinats)
+
+      "getCoordinates" in {
+        val shipOutput = ship.getCoordinates()
+      }
+
+      "getSize" in {
+        ship.getSize() should be (2)
+      }
+
+      "insertCoordinates" in {
+        // tested through getCoordinates ??
+      }
+    }
+  }
+
 }

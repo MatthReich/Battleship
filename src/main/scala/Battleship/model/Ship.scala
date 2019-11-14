6b3a53d74fc9269ddd334b6cbd62d7f92ca8d66f
Marcelg1235 <Marcelg1235@gmail.com>
2019-11-14 18:56:53 +0100

    add size +1

--- src/main/scala/Battleship/model/Ship.scala
+++ src/main/scala/Battleship/model/Ship.scala
@@ -6,20 +6,20 @@ case class Ship(shipCoordinates: Array[Int]) {
 
   def setCoordinates(): Unit = {
     if (shipCoordinates(0) == shipCoordinates(2)) {
-      size = shipCoordinates(3) - shipCoordinates(1)
+      size = shipCoordinates(3) - shipCoordinates(1) + 1
       coordinats = Array.ofDim[Int](size, 2)
       var idy = 0
       while (idy != size) {
         coordinats(idy)(0) = shipCoordinates(0)
-        coordinats(idy)(1) = shipCoordinates(1 + idy)
+        coordinats(idy)(1) = shipCoordinates(1) + idy
         idy += 1
       }
     } else {
-      size = shipCoordinates(2) - shipCoordinates(0)
+      size = shipCoordinates(2) - shipCoordinates(0) + 1
       coordinats = Array.ofDim[Int](size, 2)
       var idy = 0
       while (idy != size) {
-        coordinats(idy)(0) = shipCoordinates(0 + idy)
+        coordinats(idy)(0) = shipCoordinates(0) + idy
         coordinats(idy)(1) = shipCoordinates(1)
         idy += 1
       }
@@ -31,7 +31,7 @@ case class Ship(shipCoordinates: Array[Int]) {
     var idx = 0
     while (idx != size) {
       if (array(0) == array(2)) {
-        var idy = array(3) - array(1)
+        var idy = array(3) - array(1) + 1
         while (idy <= array(3)) {
           if (array(0) == coordinats(idx)(0) && idy == coordinats(idx)(1)) {
             output = true
@@ -39,7 +39,7 @@ case class Ship(shipCoordinates: Array[Int]) {
           idy += 1
         }
       } else if (array(1) == array(3)) {
-        var idy = array(2) - array(0)
+        var idy = array(2) - array(0) + 1
         while (idy <= array(2)) {
           if (idy == coordinats(idx)(0) && array(1) == coordinats(idx)(1)) {
             output = true

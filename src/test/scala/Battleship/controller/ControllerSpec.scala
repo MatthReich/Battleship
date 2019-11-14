c57a54e3b8cea51774433c8bd55cf076b414f31d
Marcelg1235 <Marcelg1235@gmail.com>
2019-11-14 11:13:04 +0100

    Add ControllerSpec things

--- src/test/scala/Battleship/controller/ControllerSpec.scala
+++ src/test/scala/Battleship/controller/ControllerSpec.scala
@@ -1,5 +1,23 @@
 package Battleship.controller
 
-class ControllerSpec {
+import Battleship.util.Observer
+import org.scalatest.{Matchers, WordSpec}
 
+class ControllerSpec extends WordSpec with Matchers {
+
+  "A Controller" when {
+    "observed by an Observer" should {
+      val controller = new Controller()
+      val observer = new Observer {
+        var updated: Boolean = false
+
+        def isUpdated: Boolean = updated
+
+        override def update: Unit = updated = true
+      }
+      controller.add(observer)
+
+
+    }
+  }
 }

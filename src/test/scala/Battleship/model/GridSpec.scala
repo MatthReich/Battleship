9bc1536026334017858fd584f7794b976587b953
Marcelg1235 <Marcelg1235@gmail.com>
2019-11-14 10:55:29 +0100

    Create GridSpec

---
+++ src/test/scala/Battleship/model/GridSpec.scala
@@ -0,0 +1,15 @@
+package Battleship.model
+
+import org.scalatest.{Matchers, WordSpec}
+
+class GridSpec extends WordSpec with Matchers {
+
+  "A Grid" when {
+    "new" should {
+      val grid = Grid(10)
+
+
+    }
+  }
+
+}

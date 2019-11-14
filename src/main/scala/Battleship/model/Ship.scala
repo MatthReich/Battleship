e7d5fb94e162dcf790e82ec30efb33fdbef18949
Matthias <matthreich@gmail.com>
2019-11-14 13:53:05 +0100

    reenamed function ! something does not work

--- src/main/scala/Battleship/model/Ship.scala
+++ src/main/scala/Battleship/model/Ship.scala
@@ -2,27 +2,27 @@ package Battleship.model
 
 case class Ship(shipCoordinates: Array[Int]) {
   var size = 0
-  var coordinats = Array.ofDim[Int](0, 0)
+  var coordinats = Array.ofDim[Int](5, 2)
   var idx = 0
   shipCoordinates.length match {
     case 4 => {
       coordinats = Array.ofDim[Int](2, 2)
-      insertCoord()
+      insertCoordinates()
       size = 2
     }
     case 6 => {
       coordinats = Array.ofDim[Int](3, 2)
-      insertCoord()
+      insertCoordinates()
       size = 3
     }
     case 8 => {
       coordinats = Array.ofDim[Int](4, 2)
-      insertCoord()
+      insertCoordinates()
       size = 4
     }
     case 10 => {
       coordinats = Array.ofDim[Int](5, 2)
-      insertCoord()
+      insertCoordinates()
       size = 5
     }
   }
@@ -35,7 +35,7 @@ case class Ship(shipCoordinates: Array[Int]) {
     size
   }
 
-  def insertCoord(): Unit = {
+  def insertCoordinates(): Unit = {
     var idx = 0
     for (outer <- coordinats.indices) {
       for (inner <- coordinats.indices) {

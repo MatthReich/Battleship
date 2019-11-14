647d1c8a2e74b2ab93bca1b2caaf018435ffe45d
Matthias <matthreich@gmail.com>
2019-11-14 13:27:50 +0100

    created testfile for Ship

---
+++ src/test/scala/Battleship/model/ShipSpec.scala
@@ -0,0 +1,10 @@
+package Battleship.model
+
+import org.junit.runner.RunWith
+import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class ShipSpec extends WordSpec with Matchers {
+
+}

b00bd897dbb70a074cfbdee8993fe894a7db2994
Marcelg1235 <Marcelg1235@gmail.com>
2019-11-17 14:43:47 +0100

    Add Test Class for Game

--- src/test/scala/Battleship/GameSpec.scala
+++ src/test/scala/Battleship/GameSpec.scala
@@ -1,5 +1,10 @@
 package Battleship
 
-class GameSpec {
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{Matchers, WordSpec}
+
+@RunWith(classOf[JUnitRunner])
+class GameSpec extends WordSpec with Matchers {
 
 }

9c4f56817fe01709a4d08831019b0aae7f071cd3
Marcelg1235 <Marcelg1235@gmail.com>
2019-11-17 15:25:35 +0100

    Add Test file for addShips

--- src/main/scala/Battleship/controller/Controller.scala
+++ src/main/scala/Battleship/controller/Controller.scala
@@ -13,13 +13,12 @@ class Controller() extends Observable {
   val playerGrid_02: Grid = Grid(fieldSize)
   var player_01 = Player("")
   var player_02 = Player("")
-  var nr = Array[Int](2, 0, 0, 0)
-  var nr2 = Array[Int](2, 0, 0, 0)
+  var nr: Array[Int] = Array[Int](2, 0, 0, 0)
+  var nr2: Array[Int] = Array[Int](2, 0, 0, 0)
 
 
   def addShips(grid: Grid, ship: Array[Int], nrArray: Array[Int]): Unit = {
     grid.setShip(ship, nrArray)
     notifyObservers()
   }
-
 }
--- src/test/scala/Battleship/controller/ControllerSpec.scala
+++ src/test/scala/Battleship/controller/ControllerSpec.scala
@@ -8,17 +8,23 @@ class ControllerSpec extends WordSpec with Matchers {
   "A Controller" when {
     "observed by an Observer" should {
       val controller = new Controller()
-      val observer = new Observer {
+      val observer: Observer = new Observer {
         var updated: Boolean = false
 
         def isUpdated: Boolean = updated
 
-        override def update: Boolean = {
+        override def update(): Boolean = {
           updated = true
           updated
         }
       }
       controller.add(observer)
+
+      "notify its Observer after addShips" in {
+        controller.addShips(controller.playerGrid_01, Array[Int](1, 1, 2, 1), Array[Int](1, 1, 1, 1))
+        controller.playerGrid_01.getField(1, 1) should be(1)
+        observer.update should be(true)
+      }
     }
 
   }

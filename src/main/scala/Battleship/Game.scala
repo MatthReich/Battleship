578324004b8f05a1e93ee417fcceea28c23d9e37
Marcelg1235 <Marcelg1235@gmail.com>
2019-11-17 13:55:54 +0100

    Delete Input shit in Game class

--- src/main/scala/Battleship/Game.scala
+++ src/main/scala/Battleship/Game.scala
@@ -13,17 +13,14 @@ object Game {
     tui.setPlayers
     tui.playerConfiguration()
 
-    var input: String = ""
     do {
       tui.setShipsPrint(controller.player_01, controller.playerGrid_01, controller.nr)
-      input = scala.io.StdIn.readLine()
-      tui.setShips(input, controller.playerGrid_01, controller.nr)
+      tui.setShips(controller.playerGrid_01, controller.nr)
     } while ((controller.nr(0) + controller.nr(1) + controller.nr(2) + controller.nr(3)) != 0)
 
     do {
       tui.setShipsPrint(controller.player_02, controller.playerGrid_02, controller.nr2)
-      input = scala.io.StdIn.readLine()
-      tui.setShips(input, controller.playerGrid_02, controller.nr2)
+      tui.setShips(controller.playerGrid_02, controller.nr2)
     } while ((controller.nr2(0) + controller.nr2(1) + controller.nr2(2) + controller.nr2(3)) != 0)
   }
 }

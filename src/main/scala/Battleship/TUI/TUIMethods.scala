b467ae77691bcddc86c07f143a9bbc2be747b25f
Marcelg1235 <Marcelg1235@gmail.com>
2019-11-17 14:13:25 +0100

    Change Add ship things

--- src/main/scala/Battleship/TUI/TUIMethods.scala
+++ src/main/scala/Battleship/TUI/TUIMethods.scala
@@ -27,10 +27,6 @@ object TUIMethods {
     }
   }
 
-  def input(): String = {
-    scala.io.StdIn.readLine().toString
-  }
-
   def printGetPlayer(player_01: Player, player_02: Player): String = {
     val stringPrint = new mutable.StringBuilder("")
     stringPrint ++= ("Actual player-configuration\n" +
@@ -86,6 +82,36 @@ object TUIMethods {
       idx += 1
     }
     string.toString()
+  }
+
+  def checkValidShip(input: String): Array[Int] = {
+    val inputInner = input.split(" ")
+    val tmp: Array[Int] = new Array[Int](4)
+    try {
+      if (inputInner.length == 4) {
+        var idx = 0
+        while (idx < 4) {
+          tmp(idx) = inputInner(idx).toInt
+          idx += 1
+        }
+      } else {
+        print("Format Error\n")
+        tmp(0) = 10
+      }
+    } catch {
+      case e: NumberFormatException => println("you have to input numbers\n", e)
+        tmp(0) = 10
+    }
+    if ((tmp(0) >= 0 && tmp(0) <= 9) && (tmp(1) >= 0 && tmp(1) <= 9) && (tmp(2) >= 0 && tmp(2) <= 9) && (tmp(3) >= 0 && tmp(3) <= 9)) {
+
+    } else {
+      tmp(0) = 10
+    }
+    tmp
+  }
+
+  def input(): String = {
+    scala.io.StdIn.readLine().toString
   }
 
   def output(output: String): Unit = {

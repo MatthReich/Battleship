f92ea090240d94b3576ddc28bc90340e36b279c6
Marcelg1235 <Marcelg1235@gmail.com>
2019-11-17 15:15:39 +0100

    Delete useless comments in ControllerSpec

--- src/test/scala/Battleship/controller/ControllerSpec.scala
+++ src/test/scala/Battleship/controller/ControllerSpec.scala
@@ -19,16 +19,6 @@ class ControllerSpec extends WordSpec with Matchers {
         }
       }
       controller.add(observer)
-
-      /*"notify its Observer after addShips" in {
-        var tmp = controller.addShips("1 1 1 1")
-        tmp should be(Array(1, 1, 1, 1))
-        tmp = controller.addShips("1 1 1 1 1")
-        tmp(0) should be(10)
-        tmp = controller.addShips("a 1 1 1")
-        tmp(0) should be(10)
-        observer.updated should be(true)
-      }*/
     }
 
   }

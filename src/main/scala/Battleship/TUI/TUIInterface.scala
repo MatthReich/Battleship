cdddc12c03d32089b1ac9cb7c24af9faf9abc18b
Marcelg1235 <Marcelg1235@gmail.com>
2019-11-17 13:53:20 +0100

    Make some Changes and import TUIMethods

--- src/main/scala/Battleship/TUI/TUIInterface.scala
+++ src/main/scala/Battleship/TUI/TUIInterface.scala
@@ -1,5 +1,6 @@
 package Battleship.TUI
 
+import Battleship.TUI.TUIMethods._
 import Battleship.controller.Controller
 import Battleship.model.{Grid, Player}
 import Battleship.util.Observer
@@ -10,21 +11,20 @@ class TUIInterface(controller: Controller) extends Observer {
 
   def setPlayers(): Unit = {
     controller.add(this)
-
-    print(TUIMethods.printWelcome(controller.creator_01, controller.creator_02))
-
-    controller.player_01 = TUIMethods.setPlayer(1)
-    controller.player_02 = TUIMethods.setPlayer(2)
-
+    output(printWelcome(controller.creator_01, controller.creator_02))
+    output(printSetPlayer(1))
+    controller.player_01 = setPlayer(1)
+    output(printSetPlayer(2))
+    controller.player_02 = setPlayer(2)
   }
 
   def playerConfiguration(): Unit = {
-    print(TUIMethods.printGetPlayer(controller.player_01, controller.player_02))
+    output(printGetPlayer(controller.player_01, controller.player_02))
   }
 
   def setShipsPrint(player: Player, grid: Grid, nr: Array[Int]): Unit = {
-    print(TUIMethods.printGrid(grid, player))
-    print(TUIMethods.printNrOfShips(nr))
+    output(printGrid(grid, player))
+    output(printNrOfShips(nr))
   }
 
   def setShips(input: String, grid: Grid, nr: Array[Int]): Unit = {

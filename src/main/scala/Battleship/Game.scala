31779d7f3ecc18e510344a744a9a967f4551e94f
Marcelg1235 <Marcelg1235@gmail.com>
2019-11-17 12:35:38 +0100

    Optimize Controller

--- src/main/scala/Battleship/Game.scala
+++ src/main/scala/Battleship/Game.scala
@@ -17,13 +17,13 @@ object Game {
     do {
       tui.setShipsPrint(controller.player_01, controller.playerGrid_01, controller.nr)
       input = scala.io.StdIn.readLine()
-      tui.setShips(input, controller.player_01, controller.playerGrid_01, controller.nr)
+      tui.setShips(input, controller.playerGrid_01, controller.nr)
     } while ((controller.nr(0) + controller.nr(1) + controller.nr(2) + controller.nr(3)) != 0)
 
     do {
       tui.setShipsPrint(controller.player_02, controller.playerGrid_02, controller.nr2)
       input = scala.io.StdIn.readLine()
-      tui.setShips(input, controller.player_02, controller.playerGrid_02, controller.nr2)
+      tui.setShips(input, controller.playerGrid_02, controller.nr2)
     } while ((controller.nr2(0) + controller.nr2(1) + controller.nr2(2) + controller.nr2(3)) != 0)
 
     do {

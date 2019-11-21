cac7d3c425bc6b84ab2f251ad65501865c47960f
Matthias <matthreich@gmail.com>
2019-11-21 21:45:21 +0100

    added testfile for processLine

---
+++ src/test/scala/Battleship/TUI/TUIProcessLineTest.scala
@@ -0,0 +1,10 @@
+package Battleship.TUI
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{Matchers, WordSpec}
+
+@RunWith(classOf[JUnitRunner])
+class TUIProcessLineTest extends WordSpec with Matchers {
+
+}

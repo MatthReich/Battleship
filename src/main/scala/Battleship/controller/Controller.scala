a964e41a889d0a6a812092e24ee20823aa953414
Matthias <matthreich@gmail.com>
2019-11-21 20:32:08 +0100

    changed shipsetrules for easier test

--- src/main/scala/Battleship/controller/Controller.scala
+++ src/main/scala/Battleship/controller/Controller.scala
@@ -13,8 +13,8 @@ class Controller() extends Observable {
   val playerGrid_02: Grid = Grid(fieldSize)
   var player_01 = Player("")
   var player_02 = Player("")
-  var nr: Array[Int] = Array[Int](2, 0, 0, 0)
-  var nr2: Array[Int] = Array[Int](2, 0, 0, 0)
+  var nr: Array[Int] = Array[Int](1, 0, 0, 0)
+  var nr2: Array[Int] = Array[Int](1, 0, 0, 0)
 
 
   def addShips(grid: Grid, ship: Array[Int], nrArray: Array[Int]): Unit = {

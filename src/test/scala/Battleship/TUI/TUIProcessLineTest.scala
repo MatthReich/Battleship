e54d6f0aa6684274e8d23662a831386bdbe9e965
Matthias <matthreich@gmail.com>
2019-11-21 21:50:45 +0100

    updated testfile

--- src/test/scala/Battleship/TUI/TUIProcessLineTest.scala
+++ src/test/scala/Battleship/TUI/TUIProcessLineTest.scala
@@ -1,5 +1,6 @@
 package Battleship.TUI
 
+import Battleship.model.{Grid}
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{Matchers, WordSpec}
@@ -7,4 +8,8 @@ import org.scalatest.{Matchers, WordSpec}
 @RunWith(classOf[JUnitRunner])
 class TUIProcessLineTest extends WordSpec with Matchers {
 
+  "processLineIntern" in {
+    val statement = TUIProcessLine.processLineIntern(true, "0 0", grid = Grid(10))
+    statement should be (false)
+  }
 }

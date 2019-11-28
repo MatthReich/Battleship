3ec3ba33c6906b3cb0adaa22257010a33da32224
MatthRreich <matthreich@gmail.com>
2019-11-28 11:03:57 +0100

    updated

--- src/main/scala/Battleship/Game.scala
+++ src/main/scala/Battleship/Game.scala
@@ -4,6 +4,7 @@ import Battleship.TUI.TUIInterface
 import Battleship.controller.Controller
 import Battleship.model.Grid
 
+//noinspection ScalaStyle
 object Game {
 
   val fieldSize = 10
@@ -12,7 +13,6 @@ object Game {
   controller.notifyObservers()
 
   def main(args: Array[String]): Unit = {
-
     tui.setPlayers()
     tui.playerConfiguration()
 
@@ -24,38 +24,6 @@ object Game {
       tui.setShips(controller.player_02, controller.grid_player02, controller.nr2)
     } while ((controller.nr2(0) + controller.nr2(1) + controller.nr2(2) + controller.nr2(3)) != 0)
 
-    var playerStatus = true // true = player 1, false = player2
-    var playerInput: String = ""
-    var firstTime = true
-    val gridPrint = false // grid will print without placed ships
-
-    do {
-      if (firstTime) {
-        output(printGrid(controller.playerGrid_02, controller.player_01, gridPrint))
-        println(controller.gridToString(1, gridPrint))
-        firstTime = false
-      } else { // grid nur mit spiel makierungen ausgeben
-        if (playerStatus) output(printGrid(controller.playerGrid_02, controller.player_01, gridPrint))
-        else output(printGrid(controller.playerGrid_01, controller.player_02, gridPrint))
-      }
-
-      playerInput = input()
-      playerInput match {
-        case "q" => " "
-        // cases to get some more functions like getField again or so
-
-        case _ => // grid nur mit spiel makierungen ausgeben
-          if (playerStatus) playerStatus = processLineIntern(playerStatus, playerInput, controller.playerGrid_02)
-          else playerStatus = processLineIntern(playerStatus, playerInput, controller.playerGrid_01)
-      }
-
-      if (controller.playerGrid_01.winStatement()) return
-      if (controller.playerGrid_02.winStatement()) return
-
-    } while (playerInput != "q")
 
   }
-}
-
-
 }
--- src/main/scala/Battleship/TUI/Tui.scala
+++ src/main/scala/Battleship/TUI/Tui.scala
@@ -9,15 +9,36 @@ class Tui(controller: Controller) extends Observer {
   var tui = new TUIInterface(controller)
 
   def provcessLine(input: String): Unit = {
-    if (boolean) boolean = false
 
-    input match {
-      case "q" =>
-      case "playerConfig" => tui.playerConfiguration()
-      case _ =>
-    }
+    var playerStatus = true // true = player 1, false = player2
+    var playerInput: String = ""
+    var firstTime = true
+    val gridPrint = false // grid will print without placed ships
+
+    do {
+      if (firstTime) {
+        print(controller.gridToString(1, gridPrint))
+        firstTime = false
+      } else { // grid nur mit spiel makierungen ausgeben
+        if (playerStatus) print(controller.gridToString(1, gridPrint))
+        else print(controller.gridToString(0, gridPrint))
+      }
+
+      playerInput = scala.io.StdIn.readLine().toString
+
+      playerInput match {
+        case "q" =>
+        case _ => // grid nur mit spiel makierungen ausgeben
+          if (playerStatus) playerStatus = TUIProcessLine.processLineIntern(playerStatus, playerInput, controller.grid_player02)
+          else playerStatus = TUIProcessLine.processLineIntern(playerStatus, playerInput, controller.grid_player01)
+      }
+
+      if (controller.grid_player01.winStatement()) return
+      if (controller.grid_player02.winStatement()) return
+
+    } while (playerInput != "q")
   }
-  
+
   override def update: Boolean = {
     if (boolean) {
       print(controller.gridToString(0, boolean))

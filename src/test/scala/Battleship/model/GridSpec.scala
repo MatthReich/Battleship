331cfe51c71d52fd7e4049309f8ec9a9c16b607e
Marcelg1235 <Marcelg1235@gmail.com>
2019-11-29 09:13:36 +0100

    Add Grid Tests

--- src/test/scala/Battleship/model/GridSpec.scala
+++ src/test/scala/Battleship/model/GridSpec.scala
@@ -42,15 +42,44 @@ class GridSpec extends WordSpec with Matchers {
         gridShort.setField(0, 0, 1)
         gridShort.getValue(0, 0) should be (1)
         gridShort.setField(0, 0, 0)
-        gridShort.getValue(0, 0) should be (0)
+        gridShort.getValue(0, 0) should be(0)
       }
 
       "winStatement" in {
         var check = gridShort.winStatement()
-        check should be (true)
+        check should be(true)
         gridShort.setField(0, 0, 1)
         check = gridShort.winStatement()
-        check should be (false)
+        check should be(false)
+      }
+
+      "toString" in {
+        val grid: Grid = Grid(10)
+        grid.setField(1, 1, 1)
+        grid.setField(2, 2, 2)
+        grid.setField(3, 3, 3)
+
+        var tmp = grid.toString(Player("Marcel"), sortOfPrint = true)
+
+        tmp should startWith("Field of:")
+        tmp should include("Marcel")
+        tmp should include("A")
+        tmp should include("~")
+        tmp should include(Console.GREEN + "  x  " + Console.RESET)
+        tmp should include(Console.RED + "  x  " + Console.RESET)
+        tmp should include("0")
+        tmp should endWith("\n")
+
+        tmp = grid.toString(Player("Matthias"), sortOfPrint = false)
+
+        tmp should startWith("Field of:")
+        tmp should include("Matthias")
+        tmp should include("A")
+        tmp should include("~")
+        tmp should include(Console.BLUE + "  ~  " + Console.RESET)
+        tmp should include(Console.RED + "  x  " + Console.RESET)
+        tmp should include("0")
+        tmp should endWith("\n")
       }
 
     }

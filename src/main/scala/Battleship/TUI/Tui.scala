bc37756c16b4c3d6472be6f5d0b913bd1933eb9c
MatthRreich <matthreich@gmail.com>
2019-11-29 08:37:48 +0100

    code cleanup

--- src/main/scala/Battleship/TUI/Tui.scala
+++ src/main/scala/Battleship/TUI/Tui.scala
@@ -15,15 +15,9 @@ class Tui(controller: Controller) extends Observer {
   var firstTime = true
   var shipProcess = true
 
+
   def printGrid(int: Int): Unit = {
     print(controller.gridToString(int, printGridOption))
-  }
-
-  def printFirstTimeProcessLine(): Unit = {
-    if (shipProcess) shipProcess = false
-    if (printGridOption) printGridOption = false
-
-    update
   }
 
   def printShipSetSettings(nr: Array[Int]): Unit = {
@@ -43,32 +37,53 @@ class Tui(controller: Controller) extends Observer {
     controller.addShips(player, ship)
   }
 
-  def processLine(input: String): Unit = {
+  def printFirstTimeProcessLine(): Unit = {
+    if (shipProcess) shipProcess = false
+    if (printGridOption) printGridOption = false
 
-    input match {
-        // @TODO println hier in dem Zweck erlaubt?
-      case "q" => // exit game
-      case "getPlayerConfig" => print(TUIMethods.printGetPlayer(controller.player_01, controller.player_02))
-      case "getGameStatus" => println(controller.gameStatus)
-      case _ => // grid nur mit spiel makierungen ausgeben
-        // @TODO postion von Update falsch -> muss mit enter aktualisiert werden dass richtiges feld angezeigt
-        if (playerStatus) playerStatus = controller.checkGuess(input, playerStatus, controller.grid_player02)
-        else playerStatus = controller.checkGuess(input, playerStatus, controller.grid_player01)
-        update
-    }
+    update
   }
 
   override def update: Boolean = {
     if (shipProcess) {
-      if (shipProcess) print(controller.gridToString(0, printGridOption))
-      else print(controller.gridToString(1, printGridOption))
+      if (shipProcess) {
+        print(controller.gridToString(0, printGridOption))
+      }
+      else {
+        print(controller.gridToString(1, printGridOption))
+      }
     }
-    if (playerStatus) print(controller.gridToString(1, printGridOption))
-    else print(controller.gridToString(0, printGridOption))
+    if (playerStatus) {
+      print(controller.gridToString(1, printGridOption))
+    }
+    else {
+      print(controller.gridToString(0, printGridOption))
+    }
     // @TODO ist das so okay gemacht? -> winstatmenet aus grid benutzen für gamestatus
-    if (controller.grid_player01.winStatement() || controller.grid_player02.winStatement())
+    if (controller.grid_player01.winStatement() || controller.grid_player02.winStatement()) {
       controller.gameStatus = GameStatus.SOLVED
-    else controller.gameStatus = GameStatus.IDLE
+    } else {
+      controller.gameStatus = GameStatus.IDLE
+    }
     true
   }
+
+  def processLine(input: String): Unit = {
+
+    input match {
+      // @TODO println hier in dem Zweck erlaubt?
+      case "q" => // exit game
+      case "getPlayerConfig" => print(TUIMethods.printGetPlayer(controller.player_01, controller.player_02))
+      case "getGameStatus" => print(controller.gameStatus + "\n")
+      case _ => // grid nur mit spiel makierungen ausgeben
+        if (playerStatus) {
+          playerStatus = controller.checkGuess(input, playerStatus, controller.grid_player02)
+        }
+        else {
+          playerStatus = controller.checkGuess(input, playerStatus, controller.grid_player01)
+        }
+        update
+    }
+  }
+
 }

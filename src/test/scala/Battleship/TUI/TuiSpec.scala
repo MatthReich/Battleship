12afb95430d618766859658b61e47546c25aa196
Marcelg1235 <Marcelg1235@gmail.com>
2019-11-29 09:35:51 +0100

    Add Tui Tests

--- src/test/scala/Battleship/TUI/TuiSpec.scala
+++ src/test/scala/Battleship/TUI/TuiSpec.scala
@@ -1,5 +1,47 @@
 package Battleship.TUI
 
-class TuiSpec {
+import Battleship.controller.Controller
+import Battleship.model.Grid
+import org.scalatest.{Matchers, WordSpec}
+
+class TuiSpec extends WordSpec with Matchers {
+
+  "A Tui" when {
+    "new" should {
+      var grid_1 = new Grid(10);
+      var grid_2 = new Grid(10);
+      val tui = new Tui(new Controller(grid_1, grid_2))
+      "printGrid" in {
+        tui.printGrid(0)
+      }
+
+      "printShipSetSettings" in {
+        tui.printShipSetSettings(Array[Int](1, 0, 0, 0))
+      }
+
+      "shipProcess" in {
+        tui.shipProcess("0 0 0 1", 0)
+      }
+
+      "printFirstTimeProcessLine" in {
+        tui.printFirstTimeProcessLine()
+      }
+
+      "processLine" in {
+        tui.processLine("q")
+        tui.processLine("getPlayerConfig")
+        tui.processLine("getGameStatus")
+        tui.processLine("0 0")
+        tui.playerStatus = false
+        tui.processLine("0 0")
+      }
+
+      "update" in {
+        tui.shipProcess = true
+        tui.update
+      }
+
+    }
+  }
 
 }

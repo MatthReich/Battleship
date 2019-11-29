6332ac06b2fe223cc72f2402604e28876dadd2eb
Marcelg1235 <Marcelg1235@gmail.com>
2019-11-29 09:14:19 +0100

    Add TUIInterface Tests

--- src/test/scala/Battleship/TUI/TUIInterfaceTest.scala
+++ src/test/scala/Battleship/TUI/TUIInterfaceTest.scala
@@ -1,23 +1,22 @@
-/*package Battleship.TUI
+package Battleship.TUI
 
 import Battleship.controller.Controller
-import Battleship.model.{Creator, Grid, Player}
+import Battleship.model.Grid
 import org.scalatest.{Matchers, WordSpec}
 
 class TUIInterfaceTest extends WordSpec with Matchers {
 
-  val interface = new TUIInterface(new Controller())
+  val interface = new TUIInterface(new Controller(new Grid(10), new Grid(10)))
 
   "A TUIInterface" when {
     "new" should {
 
-
-      "setPlayer" in {
-
+      "update" in {
+        val tmp = interface.update
+        tmp should be(true)
       }
 
     }
   }
 
 }
-*/

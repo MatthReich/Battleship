bb131b4c2b03ea2e030834925f9c23ad03533e7a
Matthias <matthreich@gmail.com>
2019-12-04 16:20:06 +0100

    added collide method

--- src/main/scala/Battleship/model/shipComponent/strategyCollide/StrategyCollideNormal.scala
+++ src/main/scala/Battleship/model/shipComponent/strategyCollide/StrategyCollideNormal.scala
@@ -4,7 +4,29 @@ import Battleship.model.gridComponent.advancedGrid.Grid
 import Battleship.model.shipComponent.advancedShip.Ship
 
 class StrategyCollideNormal extends StrategyCollide {
+
+
   override def collide(newShip: Ship, grid: Grid): Boolean = {
-    true
+    val shipCoordinates = newShip.getCoordinates
+    var collide = false
+    for (x <- 0 until shipCoordinates.length) {
+      for (y <- 0 until 2) {
+        if (grid.getValue(x, y) == 1)
+          if (equalsShip(x, y, shipCoordinates))
+            collide = true
+      }
+    }
+    collide
   }
+
+  private def equalsShip(getX: Int, getY: Int, coordinates: Array[Array[Int]]): Boolean = {
+    var equal = false
+    for (x <- 0 until coordinates.length) {
+      for (y <- 0 until 2) {
+        if (coordinates(x)(y) == getX && coordinates(x)(y) == getY) equal = true
+      }
+    }
+    equal
+  }
+
 }

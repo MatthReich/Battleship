c9ba9c87a8bd63bac844ac1be2cf3f5169136d75
Marcelg1235 <Marcelg1235@gmail.com>
2019-12-06 17:58:33 +0100

    Add FieldPanel things

--- src/main/scala/Battleship/aview/GUI/FieldPanel.scala
+++ src/main/scala/Battleship/aview/GUI/FieldPanel.scala
@@ -1,9 +1,42 @@
 package Battleship.aview.GUI
 
-import Battleship.controller.Controller
+import Battleship.controller.{CellChanged, Controller}
 
-import scala.swing.FlowPanel
+import scala.swing.event.MouseClicked
+import scala.swing.{BoxPanel, Color, Dimension, FlowPanel, Font, Label, Orientation, Swing}
 
-class FieldPanel(controller: Controller) extends FlowPanel {
+class FieldPanel(row: Int, column: Int, controller: Controller) extends FlowPanel {
+
+  val FieldColor = new Color(224, 224, 255)
+  val label = new Label {
+    print(row + " | " + column)
+    text = myField.toString
+    font = new Font("Verdana", 1, 36)
+  }
+  val field = new BoxPanel(Orientation.Vertical) {
+    contents += label
+    preferredSize = new Dimension(51, 51)
+    background = FieldColor
+    border = Swing.BeveledBorder(Swing.Raised)
+    listenTo(mouse.clicks)
+    listenTo(controller)
+    reactions += {
+      case e: CellChanged => {
+        label.text = myField.toString
+        repaint
+      }
+      case MouseClicked(source, point, modifiers, clicks, triggersPopup) => {
+        repaint
+      }
+    }
+  }
+
+  def redraw = {
+    contents.clear()
+    label.text = myField.toString()
+
+  }
+
+  def myField = controller.grid_player_01.getValue(row, column)
 
 }

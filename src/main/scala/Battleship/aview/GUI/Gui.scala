90bb213d17e40ca35ab6f692df2bf41efcdf5e91
Marcelg1235 <Marcelg1235@gmail.com>
2019-12-06 16:27:58 +0100

    Add CellClicked class and some variables

--- src/main/scala/Battleship/aview/GUI/Gui.scala
+++ src/main/scala/Battleship/aview/GUI/Gui.scala
@@ -1,5 +1,15 @@
 package Battleship.aview.GUI
 
-class Gui {
+import Battleship.controller.Controller
+
+import scala.swing._
+import scala.swing.event.Event
+
+class CellClicked(val row: Int, val column: Int) extends Event
+
+class Gui(controller: Controller) extends Frame {
+
+  title = "Battleship"
+  var field = Array.ofDim[FieldPanel](controller.grid_player01.size, controller.grid_player01.size)
 
 }

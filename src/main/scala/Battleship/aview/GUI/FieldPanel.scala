5ce7329f99e30d632500addab5c13a917ac4d000
Marcelg1235 <Marcelg1235@gmail.com>
2019-12-07 11:17:35 +0100

    Change a lot GUI Things

--- src/main/scala/Battleship/aview/GUI/FieldPanel.scala
+++ src/main/scala/Battleship/aview/GUI/FieldPanel.scala
@@ -3,29 +3,20 @@ package Battleship.aview.GUI
 import Battleship.controller.{CellChanged, Controller}
 
 import scala.swing.event.MouseClicked
-import scala.swing.{BoxPanel, Color, Dimension, FlowPanel, Font, Label, Orientation, Swing}
+import scala.swing.{BoxPanel, FlowPanel, Label, Orientation, Swing}
 
 class FieldPanel(row: Int, column: Int, controller: Controller) extends FlowPanel {
 
-  val FieldColor = new Color(224, 224, 255)
-  val label = new Label {
-    print(row + " | " + column)
-    text = myField.toString
-    font = new Font("Verdana", 1, 36)
-  }
   val field = new BoxPanel(Orientation.Vertical) {
-    contents += label
-    preferredSize = new Dimension(51, 51)
-    background = FieldColor
-    border = Swing.BeveledBorder(Swing.Raised)
+    contents += new Label(myField.toString)
+    border = Swing.LineBorder(java.awt.Color.BLACK, 1)
     listenTo(mouse.clicks)
     listenTo(controller)
     reactions += {
       case e: CellChanged => {
-        label.text = myField.toString
         repaint
       }
-      case MouseClicked(source, point, modifiers, clicks, triggersPopup) => {
+      case MouseClicked(source, point, modifiers, clicks, popup) => {
         repaint
       }
     }
@@ -33,10 +24,9 @@ class FieldPanel(row: Int, column: Int, controller: Controller) extends FlowPane
 
   def redraw = {
     contents.clear()
-    label.text = myField.toString()
-
+    contents += new Label(myField.toString)
+    repaint
   }
 
-  def myField = controller.grid_player_01.getValue(row, column)
-
+  def myField: Int = controller.grid_player_01.getValue(row, column)
 }
--- src/main/scala/Battleship/aview/GUI/Gui.scala
+++ src/main/scala/Battleship/aview/GUI/Gui.scala
@@ -1,15 +1,51 @@
 package Battleship.aview.GUI
 
-import Battleship.controller.Controller
+import Battleship.controller.{CellChanged, Controller}
 
 import scala.swing._
-import scala.swing.event.Event
+import scala.swing.event._
 
 class CellClicked(val row: Int, val column: Int) extends Event
 
 class Gui(controller: Controller) extends Frame {
 
-  title = "Battleship"
-  var field = Array.ofDim[FieldPanel](controller.grid_player01.size, controller.grid_player01.size)
+  listenTo(controller)
 
+  title = "Battleship"
+  val gridSize = controller.grid_player_01.size
+  var fields = Array.ofDim[FieldPanel](gridSize, gridSize)
+
+  def gridPanel = new GridPanel(gridSize, gridSize) {
+    border = Swing.LineBorder(java.awt.Color.BLACK, 1)
+    for {
+      row <- 0 until gridSize
+      column <- 0 until gridSize
+    } {
+      val fieldPanel = new FieldPanel(row, column, controller)
+      contents += fieldPanel.field
+      fields(row)(column) = fieldPanel
+      listenTo(fieldPanel)
+    }
+  }
+
+  contents = new BorderPanel {
+    add(gridPanel, BorderPanel.Position.Center)
+  }
+
+  visible = true
+  //redraw
+
+  reactions += {
+    case event: CellChanged => redraw
+  }
+
+  def redraw = {
+    for {
+      row <- 0 until gridSize
+      column <- 0 until gridSize
+    } {
+      fields(row)(column).redraw
+    }
+    repaint()
+  }
 }

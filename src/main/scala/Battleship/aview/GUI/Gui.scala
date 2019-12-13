9ec3d978ad22e19386364fbbfbd248fe79d2fca1
Marcelg1235 <Marcelg1235@gmail.com>
2019-12-13 10:48:37 +0100

    Add second grid function

--- src/main/scala/Battleship/aview/GUI/Gui.scala
+++ src/main/scala/Battleship/aview/GUI/Gui.scala
@@ -9,37 +9,24 @@ class Gui(controller: Controller) extends Frame {
 
   listenTo(controller)
 
+  var you: Boolean = true
   title = "Battleship"
   preferredSize = new Dimension(800, 600)
   val gridSize = controller.grid_player_01.size
 
-  def gridPanel = new GridPanel(gridSize + 1, gridSize) {
-    border = Swing.LineBorder(java.awt.Color.BLACK, 1)
-    for {
-      row <- 0 until gridSize + 1
-    } {
-      if (row == 0) {
-        contents += new Label("")
-      } else {
-        contents += new Label("" + row)
-      }
-    }
-    for {
-      row <- 0 until gridSize
-      column <- 0 until gridSize
-    } {
-      if (column == 0) {
-        contents += new Label("A" + (row + 1))
-
-      }
-      val fieldPanel = new FieldPanel(column, row, controller)
-      contents += fieldPanel.field
-      listenTo(fieldPanel)
+  def redraw = {
+    contents = new GridPanel(1, 2) {
+      you = true
+      contents += gridPanel
+      you = false
+      contents += gridPanel
     }
   }
 
   contents = new GridPanel(1, 2) {
+    you = true
     contents += gridPanel
+    you = false
     contents += gridPanel
   }
 
@@ -66,10 +53,28 @@ class Gui(controller: Controller) extends Frame {
     case event: CellChanged => redraw
   }
 
-  def redraw = {
-    contents = new GridPanel(1, 2) {
-      contents += gridPanel
-      contents += gridPanel
+  def gridPanel = new GridPanel(gridSize + 1, gridSize) {
+    border = Swing.LineBorder(java.awt.Color.BLACK, 1)
+    for {
+      row <- 0 until gridSize + 1
+    } {
+      if (row == 0) {
+        contents += new Label("")
+      } else {
+        contents += new Label("" + row)
+      }
+    }
+    for {
+      row <- 0 until gridSize
+      column <- 0 until gridSize
+    } {
+      if (column == 0) {
+        contents += new Label("A" + (row + 1))
+
+      }
+      val fieldPanel = new FieldPanel(you, column, row, controller)
+      contents += fieldPanel.field
+      listenTo(fieldPanel)
     }
   }
 

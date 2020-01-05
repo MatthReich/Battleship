3e5282c261a1225b743d24599444d032a58a6a78
Marcelg1235 <Marcelg1235@gmail.com>
2020-01-05 15:21:10 +0100

    Add Tests in CreatorSpec

--- src/test/scala/Battleship/model/CreatorSpec.scala
+++ src/test/scala/Battleship/model/CreatorSpec.scala
@@ -1,18 +1,25 @@
 package Battleship.model
 
+import Battleship.GameModule
+import Battleship.model.Person.InterfacePerson
+import com.google.inject.Guice
 import org.scalatest.{Matchers, WordSpec}
 
 class CreatorSpec extends WordSpec with Matchers {
-  /*
-  "A Creator" when { "new" should {
-    val creator = Creator("Your Name")
 
-    "have a name" in {
-      creator.name should be("Your Name")
-    }
+  "A Creator" when {
+    "new" should {
+      val injector = Guice.createInjector(new GameModule)
+      val creator = injector.getInstance(classOf[InterfacePerson])
 
-    "have a nice String representation" in {
-      creator.toString should be("Your Name")
+      "have a name" in {
+        creator.addName("Your Name")
+        creator.toString should be("Your Name")
+      }
+
+      "have a nice String representation" in {
+        creator.toString should be("Your Name")
+      }
     }
-  }}*/
+  }
 }

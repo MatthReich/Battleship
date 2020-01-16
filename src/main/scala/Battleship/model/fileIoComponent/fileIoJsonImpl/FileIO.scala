dfd39064b810fd24223ca2acaeab57b7d3515ec1
Matthias <matthreich@gmail.com>
2020-01-16 20:28:51 +0100

    updated save and load -> now can load only field back, saves only field

--- src/main/scala/Battleship/model/fileIoComponent/fileIoJsonImpl/FileIO.scala
+++ src/main/scala/Battleship/model/fileIoComponent/fileIoJsonImpl/FileIO.scala
@@ -6,6 +6,7 @@ import Battleship.controller.{GameState, PlayerState}
 import Battleship.model.Person.InterfacePerson
 import Battleship.model.fileIoComponent.FileIOInterface
 import Battleship.model.gridComponent.InterfaceGrid
+import Battleship.model.gridComponent.advancedGrid.Grid
 import Battleship.model.shipComponent.InterfaceShip
 import Battleship.model.shipComponent.advancedShip.Ship
 import play.api.libs.json.{JsNumber, JsValue, Json, Writes}
@@ -14,7 +15,7 @@ import scala.io.Source
 
 class FileIO extends FileIOInterface {
   override def load: (InterfaceGrid, InterfaceGrid, InterfacePerson, InterfacePerson, Array[Int], Array[Int], InterfaceShip, Array[Int], Boolean, Boolean, String, GameState, PlayerState) = {
-    var grid: InterfaceGrid = null
+    var grid1: InterfaceGrid = null
     var grid2: InterfaceGrid = null
     var player: InterfacePerson = null
     var player2: InterfacePerson = null
@@ -23,20 +24,15 @@ class FileIO extends FileIOInterface {
     var ship: Ship = null
     var shipCoordsSetting: Array[Int] = null
 
-    val source: String = Source.fromFile("grid.json").getLines.mkString
+    val source: String = Source.fromFile("saveFile.json").getLines.mkString
     val json: JsValue = Json.parse(source)
-    val size = (json \ "grid" \ "size").get.toString.toInt
-
+    val size = (json \ "grid1" \ "size").get.toString.toInt
+    grid1 = Grid()
     for (index <- 0 until size * size) {
       val row = (json \\ "row") (index).as[Int]
       val col = (json \\ "col") (index).as[Int]
-      val cell = (json \\ "cell") (index)
-      val value = (cell \ "value").as[Int]
-      // grid = grid.setField(row, col, value)
-      val given = (cell \ "given").as[Boolean]
-      val showCandidates = (cell \ "showCandidates").as[Boolean]
-      // if (given) grid = grid.setGiven(row, col, value)
-      // if (showCandidates) grid = grid.setShowCandidates(row, col)
+      val value = (json \\ "value") (index).as[Int]
+      grid1.setField(row, col, value)
     }
 
     val shipSet: Boolean = (json \ "shipSet").get.toString.toBoolean
@@ -53,25 +49,25 @@ class FileIO extends FileIOInterface {
       case PlayerState.PLAYER_TWO => PlayerState.PLAYER_TWO
     }
 
-    (grid, grid2, player, player2, shipSetting, shipSetting2, ship, shipCoordsSetting, shipSet, shipDelete, lastGuess, gameState, playerState)
+    (grid1, grid2, player, player2, shipSetting, shipSetting2, ship, shipCoordsSetting, shipSet, shipDelete, lastGuess, gameState, playerState)
   }
 
   override def save(grid: InterfaceGrid, grid2: InterfaceGrid, player: InterfacePerson, player2: InterfacePerson, shipSetting: Array[Int], shipSetting2: Array[Int], ship: InterfaceShip, shipCoordsSetting: Array[Int], shipSet: Boolean, shipDelete: Boolean, lastGuess: String, gameState: GameState, playerState: PlayerState): Unit = {
     import java.io._
-    val pw = new PrintWriter(new File("grid.json"))
-    pw.write(Json.prettyPrint(gridToJson(grid)))
-    pw.write(Json.prettyPrint(gridToJson(grid2)))
-    pw.close
+    val pw = new PrintWriter(new File("safeFile.json"))
+    pw.write(Json.prettyPrint(grid1ToJson(grid)))
+    // pw.write(Json.prettyPrint(grid2ToJson(grid2)))
+    pw.close()
   }
 
-  implicit val playerWrites = new Writes[InterfacePerson] {
-    override def writes(player: InterfacePerson): JsValue = Json.obj(
-      "player" -> player.toString
+  implicit val player = new Writes[InterfacePerson] {
+    def writes(player: InterfacePerson): JsValue = Json.obj(
+      "player" -> Json.toJson(player.toString) // @TODO functionable?
     )
   }
 
   implicit val shipSettings = new Writes[Array[Int]] {
-    override def writes(array: Array[Int]): JsValue = Json.obj(
+    def writes(array: Array[Int]): JsValue = Json.obj(
       "shipSetting" -> Json.toJson(array) // @TODO functionable?
     )
   }
@@ -120,10 +116,10 @@ class FileIO extends FileIOInterface {
   }
 
 
-  def gridToJson(grid: InterfaceGrid) = {
+  def grid1ToJson(grid: InterfaceGrid) = {
     val gridSize = 10
     Json.obj(
-      "grid" -> Json.obj(
+      "grid1" -> Json.obj(
         "size" -> JsNumber(gridSize),
         "cells" -> Json.toJson(
           for {
@@ -133,7 +129,28 @@ class FileIO extends FileIOInterface {
             Json.obj(
               "row" -> row,
               "col" -> col,
-              "cell" -> Json.toJson(grid.getValue(row, col))
+              "value" -> grid.getValue(row, col)
+            )
+          }
+        )
+      )
+    )
+  }
+
+  def grid2ToJson(grid: InterfaceGrid) = {
+    val gridSize = 10
+    Json.obj(
+      "grid2" -> Json.obj(
+        "size" -> JsNumber(gridSize),
+        "cells" -> Json.toJson(
+          for {
+            row <- 0 until gridSize
+            col <- 0 until gridSize
+          } yield {
+            Json.obj(
+              "row" -> row,
+              "col" -> col,
+              "value" -> Json.toJson(grid.getValue(row, col))
             )
           }
         )

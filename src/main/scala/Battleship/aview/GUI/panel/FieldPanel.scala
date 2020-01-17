98743ddbe2ca088076038feb626902cd6aa29a52
Matthias <matthreich@gmail.com>
2020-01-17 13:45:44 +0100

    implemented guessing

--- src/main/scala/Battleship/aview/GUI/panel/FieldPanel.scala
+++ src/main/scala/Battleship/aview/GUI/panel/FieldPanel.scala
@@ -5,20 +5,20 @@ import java.awt.Color
 import Battleship.controller.{CellChanged, InterfaceController, PlayerState}
 
 import scala.swing.event.UIEvent
-import scala.swing.{BoxPanel, FlowPanel, Orientation, Swing}
+import scala.swing.{BoxPanel, FlowPanel, Label, Orientation, Swing}
 
 class FieldPanel(you: Boolean, column: Int, row: Int, controller: InterfaceController) extends FlowPanel {
 
   val field = new BoxPanel(Orientation.Vertical) {
     myField match {
       case 0 => background = Color.BLUE
-        contents += new CoordLabel(column, row, "~")
+        contents += new Label("~")
       case 1 => background = Color.GREEN
-        contents += new CoordLabel(column, row, "x")
+        contents += new Label("x")
       case 2 => background = Color.RED
-        contents += new CoordLabel(column, row, "x")
+        contents += new Label("x")
       case 3 => background = new Color(148, 197, 229)
-        contents += new CoordLabel(column, row, "0")
+        contents += new Label("0")
     }
     border = Swing.LineBorder(java.awt.Color.BLACK, 1)
     listenTo(mouse.clicks)
@@ -30,14 +30,13 @@ class FieldPanel(you: Boolean, column: Int, row: Int, controller: InterfaceContr
       case event: UIEvent => {
         print(event.source + "\n")
         event.source.background = Color.GRAY
-        val x = event.source.asInstanceOf[CoordLabel].getX()
-        val y = event.source.asInstanceOf[CoordLabel].getY()
-        val string: String = x + y + ""
-        var grid = controller.getGridPlayer1
+        val x = column
+        val y = row
+        val string: String = x + " " + y
         controller.getPlayerState match {
-          case PlayerState.PLAYER_TWO => grid = controller.getGridPlayer2
+          case PlayerState.PLAYER_ONE => controller.checkGuess(string, controller.getGridPlayer2)
+          case PlayerState.PLAYER_TWO => controller.checkGuess(string, controller.getGridPlayer1)
         }
-        controller.checkGuess(string, grid)
         repaint
       }
     }
